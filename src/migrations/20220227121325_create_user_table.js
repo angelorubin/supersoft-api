12fac126712c8097f72a316acc7b8f16cd8bec68
angelorubin <angelorubin@gmail.com>
2022-03-01 23:16:34 -0300

    Criando schema dos caampos da tabela user

--- src/migrations/20220227121325_create_user_table.js
+++ src/migrations/20220227121325_create_user_table.js
@@ -3,17 +3,19 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-	knex.schema.createTable("user", (table) => {
-		table.uuid("id").primary();
-		table.string("email");
-		table.string("password");
-		table.enum("role", ["basic", "supervisor", "admin"]);
-		table.string("accessToken");
-	});
+  knex.schema.createTable("user", (table) => {
+    table.increments("id");
+    table.string("email");
+    table.string("password");
+    table.enum("role", ["basic", "supervisor", "admin"]).defaultTo("basic");
+    table.string("accessToken");
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {};
+exports.down = function (knex) {
+  knex.schema.dropTable("user");
+};
