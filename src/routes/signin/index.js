7372b626afbeb5b61f48c5d4c17d10d2794eba61
Angelo Rubin <angelorubin@gmail.com>
2022-03-05 10:03:33 -0300

    Criando controller para signin

--- src/routes/signin/index.js
+++ src/routes/signin/index.js
@@ -1,16 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const { signin } = require("src/controllers/signin");
 
-router.get("/", async (req, res, next) => {
-	const users = await getUsers();
-	console.log(users);
-	res.json({ message: "get route signin" });
-});
-
-router.post("/", async function (req, res, next) {
-	const { email, password } = req.body;
-
-	res.json({ email, password });
-});
+router.post("/", signin);
 
 module.exports = router;
