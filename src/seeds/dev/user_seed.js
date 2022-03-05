a4fac72712a9e0b302fdfbf383bfc7965cf9c6b4
Angelo Rubin <angelorubin@gmail.com>
2022-03-05 10:06:54 -0300

    Criando seeds para testar a tabela user

--- src/seeds/dev/user_seed.js
+++ src/seeds/dev/user_seed.js
@@ -3,11 +3,14 @@
  * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
-  // Deletes ALL existing entries
-  await knex("user").del();
-  await knex("user").insert([
-    { id: 1, colName: "rowValue1" },
-    { id: 2, colName: "rowValue2" },
-    { id: 3, colName: "rowValue3" },
-  ]);
+	// Deletes ALL existing entries
+	await knex("user").del();
+	await knex("user").insert([
+		{
+			email: "angelorubin@gmail.com",
+			password: "master77",
+			role: "admin",
+			access_token: "",
+		},
+	]);
 };
