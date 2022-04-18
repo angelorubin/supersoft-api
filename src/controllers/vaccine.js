f8ecb4a7c1eec7ea9da556453c4e1ac88f4ae68c
Angelo Rubin <angelorubin@gmail.com>
2022-04-18 18:16:16 -0300

    Implementando controller retrieveVaccines

--- src/controllers/vaccine.js
+++ src/controllers/vaccine.js
@@ -1,82 +1,92 @@
 const Vaccine = require("src/models/vaccine");
 const bookshelf = require("src/config/db");
 
-exports.index = async (req, res, next) => {
-  try {
-    res.json({
-      message: "vaccine index",
-    });
-  } catch (error) {
-    next(error);
-  }
+exports.retrieveVaccines = async (req, res, next) => {
+	try {
+		const results = await new Vaccine().fetchPage({}).then(function (results) {
+			return results;
+		});
+
+		res.json({
+			results,
+		});
+	} catch (error) {
+		next(error);
+	}
 };
 
 exports.create = async (req, res, next) => {
-  bookshelf.transaction(async (trx) => {
-    try {
-      const {
-        nome_vacina,
-        nome_fabricante,
-        pais_origem,
-        quantidade_minimas_doses,
-        percentual_eficacia_comprovada,
-        preco_venda_por_dose,
-      } = req.body;
+	bookshelf.transaction(async (trx) => {
+		try {
+			const {
+				nomeVacina,
+				nomeFabricante,
+				paisOrigem,
+				quantidadeMinimasDoses,
+				percentualEficaciaComprovada,
+				precoVendaPorDose,
+			} = req.body.data;
 
-      let data = {
-        nome_vacina,
-        nome_fabricante,
-        pais_origem,
-        quantidade_minimas_doses,
-        percentual_eficacia_comprovada,
-        preco_venda_por_dose,
-      };
+			let data = {
+				nome_vacina: nomeVacina,
+				nome_fabricante: nomeFabricante,
+				pais_origem: paisOrigem,
+				quantidade_minimas_doses: quantidadeMinimasDoses,
+				percentual_eficacia_comprovada: percentualEficaciaComprovada,
+				preco_venda_por_dose: precoVendaPorDose,
+			};
 
-      return Vaccine.forge(data)
-        .save(null, { transacting: trx })
-        .then(() => {
-          trx.commit;
-          res.json({ message: "A vacina foi cadastrada com sucesso" });
-        })
-        .catch((e) => {
-          trx.rollback;
-          res.json({
-            message: "Ocorreu um erro, a vacina não foi cadastrada.",
-          });
-        });
-    } catch (error) {
-      await trx.rollback;
-      res.json({ error });
-    }
-  });
+			return Vaccine.forge(data)
+				.save(null, { transacting: trx })
+				.then(() => {
+					trx.commit;
+					res.json({
+						status: "ok",
+						message: "A vacina foi cadastrada com sucesso",
+					});
+				})
+				.catch((e) => {
+					res.json({
+						status: "error",
+						message: "Ocorreu um erro, a vacina não foi cadastrada.",
+					});
+					trx.rollback;
+				});
+		} catch (error) {
+			await trx.rollback;
+			res.json({ status: "error", message: error });
+		}
+	});
 };
 
 exports.retrieveVaccineById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
+	try {
+		const { id } = req.params;
 
-    const vaccine = await new Vaccine({ id })
-      .fetch()
-      .then((vaccine) => vaccine);
+		const vaccine = await new Vaccine({ id })
+			.fetch()
+			.then((vaccine) => vaccine);
 
-    res.json({ vaccine });
-  } catch (error) {
-    res.json({ error });
-  }
+		res.json({ vaccine });
+	} catch (error) {
+		res.json({ error });
+	}
 };
 
 exports.update = async (req, res, next) => {
-  try {
-    const { id } = req.params;
+	try {
+		const { id } = req.params;
 
-    const { nome_vacina } = req.body;
+		const { nome_vacina } = req.body;
 
-    const vaccine = await Vaccine.where({ id })
-      .save({ nome_vacina }, { patch: true })
-      .then((vaccine) => vaccine);
+		const vaccine = await Vaccine.where({ id })
+			.save({ nome_vacina }, { patch: true })
+			.then((vaccine) => vaccine);
 
-    res.json({ vaccine });
-  } catch (error) {
-    res.json({ error });
-  }
+		res.json({ vaccine });
+	} catch (error) {
+		res.json({ error });
+	}
 };
+
+exports.destroy = async (req, res, next) => {};
--- src/routes/vaccine/index.js
+++ src/routes/vaccine/index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const {
-  index,
-  create,
-  retrieveVaccineById,
-  update,
+	retrieveVaccines,
+	create,
+	retrieveVaccineById,
+	update,
+	destroy,
 } = require("src/controllers/vaccine");
 
-router.get("/", index);
+router.get("/", retrieveVaccines);
 router.post("/", create);
 router.get("/:id", retrieveVaccineById);
 router.patch("/:id", update);
-// router.delete("/:id", '');
+router.delete("/:id", destroy);
 
 module.exports = router;
