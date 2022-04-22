5dc5c1b1c7dd97973824e60150c3e0d6e28f27b5
angelorubin <angelorubin@gmail.com>
2022-04-21 23:58:58 -0300

    Modificando router.put para router.delete

--- src/routes/vaccine/index.js
+++ src/routes/vaccine/index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const {
-	retrieveVaccines,
-	create,
-	retrieveVaccineById,
-	update,
-	destroy,
+  retrieveVaccines,
+  create,
+  retrieveVaccineById,
+  update,
+  destroy,
 } = require("src/controllers/vaccine");
 
 router.get("/", retrieveVaccines);
 router.post("/", create);
 router.get("/:id", retrieveVaccineById);
 router.patch("/:id", update);
-router.put("/:id", destroy);
+router.delete("/:id", destroy);
 
 module.exports = router;
